fe27eb1130b121d1bc16801fe63bcf85738117ef
Johnylab <joaobatistacomunic@hotmail.com>
2019-03-21 23:42:54 -0300

    urls sem extensão

--- server/enviar_email.php
+++ server/enviar_email.php
@@ -36,19 +36,19 @@ if ($_POST['nome'] !== '' && $_POST['email'] !== '' && $_POST['telefone'] !== ''
 	//Enviando o email
 	if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){
 		if ($isOrc){
-			header('Location: http://' . $my_domain . '/orcamento_enviado.html');
+			header('Location: http://' . $my_domain . '/orcamento_enviado/');
 			exit('orçamento enviado');
 		}
-		header('Location: http://' . $my_domain . '/email_enviado.html');
+		header('Location: http://' . $my_domain . '/email_enviado/');
 		exit('email enviado');
 	}
 	else{
-		header('Location: http://' . $my_domain . '/email_com_erro.html');
+		header('Location: http://' . $my_domain . '/email_com_erro/');
 		exit('email com erro');
 	}
 }
 else {
-	header('Location: http://' . $my_domain . '/email_incompleto.html');
+	header('Location: http://' . $my_domain . '/email_incompleto/');
 	exit('dados faltando');
 }
 ?>
